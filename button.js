2be7c9788b1c5cf3132a56f0c4bf1e3fdc6bcbb0
Patrick <pbentley07@gmail.com>
2017-03-07 20:12:52 -0500

    Fixed Loop for Images

--- button.js
+++ button.js
@@ -6,7 +6,7 @@ var counter = 10;
 var starters = ["boondocks", "issa rae", "afro", "black people", "atlanta", "chapelle"];
 
 var button = $('<button>');
-var image = $('<img>');
+
 
   for (var i=0; i < starters.length;i++){
     var button = $('<button/>');
@@ -27,6 +27,7 @@ var image = $('<img>');
 
 
     $(".gifRequest").on("click", function() {
+          $('#gifSpace').empty();
         var searchCat = $(this).attr('data-category');
         console.log(searchCat);
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
@@ -41,6 +42,7 @@ var image = $('<img>');
             results = response.data;
             console.log(results);
             for(var i = 0;i < results.length; i++){
+              var image = $('<img>');
               $('#gifSpace').append("Boom!!");
               $(image).attr('src', results[i].images.fixed_height_still.url);
               $('#gifSpace').append(image);
